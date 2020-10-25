c43848f9d929fe9029da3e5b130bb5548318acc4
ShaxxKh <sop.ugc2@gmail.com>
2020-10-25 18:37:57 +0500

    feat: implemented a function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function decodeWord(word) {
+    let eachLetterNums
+    let decodedWord = ''
+    for(let i = 0; i < word.length; i = i + 10) {
+        let eachLetterEncoded = ''
+        eachLetterNums = word.slice(i, i + 10)
+        for(let j = 0; j < eachLetterNums.length; j = j + 2) {
+            if(eachLetterNums[j] + eachLetterNums[j + 1] === '10'){
+                eachLetterEncoded += '.'
+            }
+            if(eachLetterNums[j] + eachLetterNums[j + 1] === '11'){
+                eachLetterEncoded += '-'
+            }
+        }
+        decodedWord += MORSE_TABLE[eachLetterEncoded]
+    }
+    return decodedWord
+}
+
 function decode(expr) {
-    // write your solution here
+    let words = expr.split('**********')
+    decodedWordsArray = words.map(word => decodeWord(word))
+    decodedStr = decodedWordsArray.join(' ')
+    return decodedStr
+    // split
+    // loop & decode word
+    // join
+    // return
 }
 
 module.exports = {
